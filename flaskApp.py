b03ce81405e9e5d4ab5ca11434eec3e4a35ec835
Aarav Wadhwani <66585793+aarav-wadhwani@users.noreply.github.com>
2024-07-03 00:34:16 +0530

    left some comments for @hemzp

--- flaskApp.py
+++ flaskApp.py
@@ -12,7 +12,15 @@ def generate_pdf():
     topic = data.getlist('topic')
     years = data.getlist('year')
 
-    #call db client here
+    # @hemil can you do this, I'm a little unsure on how to use the new client you made.
+    """ possible approach:
+        - copy all classes that client uses, and the client, into this directory.
+        - create a function in the client that takes in the user input as parameters,
+          generates a pdf, and returns the path to the output pdf.
+    """
+    # @hemil once you've done this, let me know and I'll connect the client to this app 
+    # and proceed with hosting it. If its more convenient for you, I can shift this to the main repo.
+
 
     output_pdf_path = f'{course}-{professor}-{topic}.pdf'
 
